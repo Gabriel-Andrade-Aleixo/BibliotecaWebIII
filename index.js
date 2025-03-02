ec3b7fc8d5d5cfcb3292ad98ca7d00c494eb47c9
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2025-03-02 13:43:42 -0300

    index

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const index = require('./api/routes/indexRoutes')
+const usuarios = require('./api/routes/UserRoutes')
+const livros = require('./api/routes/LivroRoutes')
+const autores = require('./api/routes/AutorRoutes')
+const emprestimos = require('./api/routes/EmprestimoRoutes')
+
+app.use('/usuarios', usuarios);
+app.use('/livros', livros);
+app.use('/emprestimos', emprestimos);
+app.use('/autores', autores);
+app.use('/', index);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
